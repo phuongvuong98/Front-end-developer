d80db36b3668cd00aeb8ec63b6afa69b5fa27ba2
Apple <apple@Apples-MacBook-Pro.local>
2019-01-19 00:04:24 +0700

    Add the VSG-initialized

--- full-stack-dev/[Udemy]nodejs-complete-guides/[SEC-6]Work-dynamicContent-and-add-TempEng/Starter/2--Starter/app.js
+++ full-stack-dev/[Udemy]nodejs-complete-guides/[SEC-6]Work-dynamicContent-and-add-TempEng/Starter/2--Starter/app.js
@@ -9,6 +9,36 @@ const adminData = require('./routers/admin');
 const shopRouter = require('./routers/shop');
 
 
+// we did install express handlebars but this actually is a package that not auto-installed by express
+// so instead we manually have to tell express that there is such an express handlebars engine available.
+const expressHbs = require('express-handlebars');
+
+
+// now we have to tell express that this exists
+// we call engine method and this registers a new templating engine  
+// in case we're using one which is not built-in, the pug was built-in kind of,
+// express-handlebars is not.
+// arg1: name what you want
+// arg2: tool you use, that turns out to be a function
+// returns the intialised view engine
+// app.engine('hbs', expressHbs({layoutsDir: 'views/layouts', defaultLayout: 'main-layout', extname: 'hbs'}));
+// app.set('view engine', 'hbs');
+// app.set('views', 'views');
+
+
+
+// view engine allow us to tell express hey for any dynamic templates we're trying to render
+// and there will be special function
+// app.set('view engine', 'pug');
+// app.set('views', 'views');
+
+
+// by setting this special view engine configuration, a reserved configuration which is understood.
+app.set('view engine', 'ejs');
+// we alse tell expressjs where our views are to be found though that would be default setting here by away.
+// you don't need to add this if you do use views folder.
+app.set('views', 'views');
+
 // import path to find file easily
 const path = require('path');
 
@@ -30,12 +60,19 @@ app.use(bodyParser.urlencoded({extended: true}));
 // user should be able to access public folder and
 app.use(express.static(path.join(rootPath, 'public')));
 
+
 app.use('/admin', adminData.router);
 app.use(shopRouter);
 
 // if you don't use path, path will be '/' and don't care about method 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(rootPath, 'views', '404.html'));
+    //res.status(404).sendFile(path.join(rootPath, 'views', '404.html'));
+    //res.status(404).render('404', {pageTitle: 'Path not found'}); => pug
+    //res.status(404).render('hbs/404', {pageTitle: 'Path not found'}); => handlebars
+    res.status(404).render('ejs/404.ejs', {pageTitle: 'Path not found',
+        activeShop: false,
+        activeProduct: false}
+    );
 });
 
 app.listen(3000);
--- full-stack-dev/[Udemy]nodejs-complete-guides/[SEC-6]Work-dynamicContent-and-add-TempEng/Starter/2--Starter/routers/admin.js
+++ full-stack-dev/[Udemy]nodejs-complete-guides/[SEC-6]Work-dynamicContent-and-add-TempEng/Starter/2--Starter/routers/admin.js
@@ -17,13 +17,31 @@ router.get('/add-product',(req, res, next) => {
     // use path because you don't care format in OS which is MAC OS or WIN
     // __dirname: this is a global variable which simply holds the absolute path on our OS.
     // don't use slash('/') because path automatic join any thinh you path.
-    res.sendFile(path.join( rootPath, 'views', 'add-product.html'));
+    //res.sendFile(path.join( rootPath, 'views', 'add-product.html'));
+
+    //res.render('pug/add-product', {pageTitle: "Add product", path: '/admin/add-product'} => pug
+    // res.render('hbs/add-product', {pageTitle: "Add product", path: '/admin/add-product',
+    //     activeShop: false,
+    //     activeProduct: true,
+    //     formCSS: true,
+    //     productCSS: true
+    // }) 
+    
+
+
+    // we did install ejs and then you always just refer to the name of your view
+    res.render('ejs/add-product.ejs', {pageTitle: "Add product", path: '/admin/add-product',
+        activeShop: false,
+        activeProduct: true,
+        formCSS: true,
+        productCSS: true
+    }) 
+
 });
 
 // => /admin/add-product vs POST method
 router.post('/add-product', (req, res, next) => {
-    console.log(req.body);
-    products.push({title: req.body.product});
+    products.push({title: req.body.title});
     //products.push({title: 1});
     console.log(products);
     res.redirect('/');
